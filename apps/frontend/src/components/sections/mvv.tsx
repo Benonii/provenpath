78f0d37be09dcf150575db01d63b77cbceaeefaa
BENONI ESCKINDER <besckinder@gmail.com>
2026-01-13 13:21:02 +0300

    fix: Layout

--- apps/frontend/src/components/sections/mvv.tsx
+++ apps/frontend/src/components/sections/mvv.tsx
@@ -53,32 +53,32 @@ const MVV: React.FC = () => {
           </div>
 
           {/* Right Side - Image Grid Layout */}
-          <div className="grid grid-cols-4 grid-rows-6 gap-4 h-[600px] md:h-[800px] w-full">
+          <div className="grid grid-cols-6 grid-rows-6 gap-4 h-[650px] md:h-[650px] w-full">
             {/* First Image: two rows, first two columns */}
-            <div className="row-span-2 col-span-2 relative">
+            <div className="row-span-3 col-span-3 relative ">
               <img 
                 src={mvv1} 
                 alt="MVV 1" 
-                className="w-full h-full rounded-[2rem] shadow-lg object-cover"
+                className="w-full h-full rounded-lg object-cover"
               />
             </div>
 
             {/* Second Image: next two rows, next two columns */}
-            <div className="row-start-3 row-span-2 col-start-3 col-span-2 relative">
+            <div className="row-start-2 row-span-3 col-start-4 col-span-5 relative bg-trasparent!">
               <div className="absolute -top-8 right-0 bg-white/40 backdrop-blur-md p-3 rounded-xl shadow-md border border-white/40 max-w-[150px] z-30">
-                <p className="text-[9px] leading-tight text-gray-800 font-medium">
+                <p className="text-xs leading-tight text-gray-800 font-medium">
                   Lorem ispum Lorem ispumLorem ispumLorem ispum
                 </p>
               </div>
               <img 
                 src={mvv2} 
                 alt="MVV 2" 
-                className="w-full h-full rounded-[2rem] shadow-xl object-cover"
+                className="w-full h-full rounded-lg object-cover"
               />
             </div>
 
             {/* Third Image: next two rows, first 3 columns */}
-            <div className="row-start-5 row-span-2 col-start-1 col-span-3 relative">
+            <div className="row-start-5 row-span-6 col-start-1 col-span-5 relative">
               <div className="absolute -top-8 left-0 bg-white/40 backdrop-blur-md p-3 rounded-xl shadow-md border border-white/40 max-w-[200px] z-40">
                 <p className="text-[9px] leading-tight text-gray-800 font-medium">
                   Lorem ipsum orem ipsum orem ipsum orem ipsum
@@ -87,7 +87,7 @@ const MVV: React.FC = () => {
               <img 
                 src={mvv3} 
                 alt="MVV 3" 
-                className="w-full h-full rounded-[2rem] shadow-xl object-cover"
+                className="w-full h-full rounded-[2rem] object-cover"
               />
             </div>
           </div>
