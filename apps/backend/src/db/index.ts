ca05668bc6d5749f960170274a8bdda0c33d4264
BENONI ESCKINDER <besckinder@gmail.com>
2026-01-14 15:56:30 +0300

    feat: Simplified backend

--- apps/backend/src/db/index.ts
+++
@@ -1,8 +0,0 @@
-import { drizzle } from "drizzle-orm/postgres-js";
-import postgres from "postgres";
-import * as schema from "./schema";
-import { env } from "@/utils/env";
-
-const client = postgres(env.DATABASE_URL!, { max: 1 });
-
-export const db = drizzle(client, { schema });
